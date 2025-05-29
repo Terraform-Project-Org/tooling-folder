687603abbd6f5a9817f80e78d0791121ebb0490d
karin <ovbkarin@gmail.com>
2025-05-29 14:11:27 -0400

    Update: added changes to development branch

--- backend.tf
+++ backend.tf
@@ -2,8 +2,19 @@ terraform {
   required_version = ">=0.12.0"
   backend "s3" {
     key            = "tooling/terraform.state"
-    bucket         = "class38-terraform-backend-bucket"
-    region         = "us-east-2"
+    bucket         = "class38-terraform-backend-01"
+    region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
   }
 }
+
+
+# terraform {
+#   required_version = ">=0.12.0"
+#   backend "s3" {
+#     key            = "prod/terraform.state"
+#     bucket         = "class38-terraform-backend-01"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform-state-locking"
+#   }
+# }
